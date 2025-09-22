4b4d2fb9153b7e762625478b582d06dae574ae7c
thesonproductions <thesontpst@gmail.com>
2025-09-22 11:31:48 +0700

    9 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,37 @@ const root = document.documentElement;
     root.dataset.theme = next; localStorage.setItem('theme', next);
     setTimeout(()=>btn.classList.remove('is-toggling'), 380);
   };
+
+   // Bật chế độ animation có JS
+   document.body.classList.add('has-io');
+
+   // Stagger nhẹ cho từng mục
+   document.querySelectorAll('.timeline .t').forEach((el, i) => {
+     el.style.setProperty('--stagger', i);
+   });
+ 
+   // Vẽ đường dọc khi timeline vào khung nhìn
+   const tl = document.querySelector('.timeline');
+   if (tl) {
+     const ioLine = new IntersectionObserver((ents)=>{
+       ents.forEach(ent=>{
+         if(ent.isIntersecting){
+           tl.classList.add('is-in');
+           ioLine.unobserve(tl);
+         }
+       });
+     }, { threshold: .12 });
+     ioLine.observe(tl);
+   }
+ 
+   // Reveal từng item
+   const ioItems = new IntersectionObserver((ents)=>{
+     ents.forEach(ent=>{
+       if(ent.isIntersecting){
+         ent.target.classList.add('seen');
+         ioItems.unobserve(ent.target);
+       }
+     });
+   }, { threshold:.18, rootMargin: '0px 0px -10% 0px' });
+ 
+   document.querySelectorAll('.timeline .t').forEach(el => ioItems.observe(el));
